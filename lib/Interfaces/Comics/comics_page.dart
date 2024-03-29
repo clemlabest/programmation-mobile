1e7c5e659d6aee17434b5bfa079ef4065d47d982
Igorstr78 <103126531+Igorstr78@users.noreply.github.com>
2024-03-29 11:31:32 +0100

    finalisationComicsPage

--- lib/Interfaces/Comics/comics_page.dart
+++ lib/Interfaces/Comics/comics_page.dart
@@ -23,13 +23,13 @@ class _MyComicsPageState extends State<MyComicsPage> {
   Future<void> fetchComics() async {
     try {
       final data = await apiClient.getEndpointData('issues');
-      if (!mounted) return; // Vérifiez si le widget est toujours monté avant de mettre à jour l'état
+      if (!mounted) return;
       setState(() {
         comicsList = data['results'];
         isLoading = false;
       });
     } catch (e) {
-      if (!mounted) return; // Vérifiez aussi ici si le widget est toujours monté
+      if (!mounted) return;
       setState(() {
         isLoading = false;
       });
@@ -43,30 +43,71 @@ class _MyComicsPageState extends State<MyComicsPage> {
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
           : ListView.separated(
+              padding: const EdgeInsets.all(15), // Padding global pour la liste
               itemCount: comicsList.length,
-              separatorBuilder: (context, index) => const SizedBox(height: 20), // Plus grand espace entre les éléments
+              separatorBuilder: (context, index) => const Divider(color: Colors.transparent, height: 20),
               itemBuilder: (context, index) {
                 final comic = comicsList[index];
                 final imageInfo = comic['image'] ?? {};
-                final thumbnailUrl = imageInfo['small_url'] ?? 'https://placekitten.com/200/300'; // URL par défaut ou une image placeholder
+                final thumbnailUrl = imageInfo['small_url'] ?? 'https://placekitten.com/200/300';
+                
                 return Card(
-                  color: const Color(0xFF284C6A), // Arrière-plan de l'élément
+                  color: const Color(0xFF1E3243),
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0), // Padding intérieur pour la carte
+                    padding: const EdgeInsets.all(8.0),
                     child: Row(
-                      children: <Widget>[
-                        Image.network(thumbnailUrl, width: 100, height: 150, fit: BoxFit.cover), // L'image ne prendra pas toute la carte
-                        const SizedBox(width: 10), // Espace entre l'image et le texte
+                      crossAxisAlignment: CrossAxisAlignment.start, // Align items to the start of the row
+                      children: [
+                        Stack(
+                          alignment: Alignment.topLeft,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10, bottom: 10, left: 10), // Ajout du padding autour de l'image
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10), // Bords arrondis avec un radius de 10
+                                child: Image.network(
+                                  thumbnailUrl, 
+                                  width: 140, // Largeur fixée
+                                  height: 140, // Hauteur fixée pour garder un aspect carré
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              left: 0,
+                              top: 0,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                                decoration: BoxDecoration(
+                                  color: const Color.fromARGB(255, 227, 139, 9),
+                                  borderRadius: BorderRadius.circular(20), // Plus arrondi pour un ovale
+                                ),
+                                child: Text(
+                                  '#${index + 1}',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(width: 15), // Espacement entre l'image et le texte
                         Expanded(
-                          child: ListTile(
-                            title: Text(
-                              comic['name'] ?? 'Titre inconnu',
-                              style: const TextStyle(fontSize: 17, color: Colors.white), // Taille du titre à 17px
-                            ),
-                            subtitle: Text(
-                              'Issue #${comic['issue_number'].toString()}',
-                              style: const TextStyle(color: Colors.white70),
-                            ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start, // Aligner le texte à gauche
+                            children: [
+                              Text(
+                                comic['name'] ?? 'Titre inconnu',
+                                style: const TextStyle(fontSize: 17, color: Colors.white, fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(height: 5), // Petit espace entre le titre et le sous-titre
+                              Text(
+                                'Issue #${comic['issue_number'].toString()}',
+                                style: const TextStyle(fontSize: 15, color: Colors.white70),
+                              ),
+                            ],
                           ),
                         ),
                       ],
